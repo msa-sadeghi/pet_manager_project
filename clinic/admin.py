7389f94eaee79e23bf568ec26f36a9e8d51d6de4
mostafa ranjbar <mostafaranjbar1389@email.com>
2026-05-11 16:35:06 +0330

    add login page

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Vaccine, VetVisit
 
-# Register your models here.
+
+@admin.register(Vaccine)
+class VaccineAdmin(admin.ModelAdmin):
+    list_display = ["pet", "name", "date_given", "next_due", "administered_by"]
+    list_filter = ["name", "date_given"]
+    search_fields = ["pet__name", "name"]
+
+
+@admin.register(VetVisit)  
+class VetVisitAdmin(admin.ModelAdmin):
+    list_display = ['pet', 'visit_date', 'visit_type', 'vet_name', 'cost']
+    list_filter = ['visit_type', 'visit_date']
+    search_fields = ['pet__name', 'vet_name']
