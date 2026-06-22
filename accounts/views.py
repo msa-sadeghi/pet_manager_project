6d43306e6558bdfeee14c311466428819fbd0f06
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-06-22 18:58:22 +0330

    login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .forms import RegistrationForm, UserUpdateForm, OwnerUpdateForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def register_view(request):
     form = RegistrationForm()
     return render(request, "accounts/register.html", {"form": form})
+
+
+def login_view(request):
+    form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
