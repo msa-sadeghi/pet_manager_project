85a8c79d29fc6fb60600d832effb0b6b98622806
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-06-29 19:00:06 +0330

    s

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm, UserUpdateForm, OwnerUpdateForm
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
 
 def register_view(request):
-    form = RegistrationForm()
+    if request.user.is_authenticated:
+        return redirect("pets:pet_list")
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            print(form.errors)
+            user = form.save()
+            login(request, user)
+    else:
+        form = RegistrationForm()
     return render(request, "accounts/register.html", {"form": form})
 
 
 def login_view(request):
-    form = AuthenticationForm()
+
+    if request.user.is_authenticated:
+        return redirect("pets:pet_list")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("pets:pet_list")
+    else:
+        form = AuthenticationForm()
     return render(request, "accounts/login.html", {"form": form})
+
+
+def logout_view(request):
+    if request.method == "POST":
+
+        logout(request)
+
+    return redirect("accounts:login")
--- pets/views.py
+++ pets/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Pet
+
+
+@login_required
 def view_all_pets(request):
     all_pets = Pet.objects.all()
-    context = {
-        'pets' : all_pets
-    }
-    return render(request, 'pets/pets_info.html', context)
+    context = {"pets": all_pets}
+    return render(request, "pets/pets_info.html", context)
